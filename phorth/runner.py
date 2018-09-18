d4cc4d80be1928a96994e62580135775dc03cf2d
Joe Jevnik <joe@quantopian.com>
2018-09-17 23:41:48 -0400

    ENH: update copyright

--- phorth/runner.py
+++ phorth/runner.py
@@ -15,7 +15,7 @@ version = '0.2.0'
 
 _header = """\
 phorth {version}
-Copyright (C) 2016 Joe Jevnik
+Copyright (C) 2018 Joe Jevnik
 License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.  Type "_license"
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def extension(name):
 
 setup(
     name='phorth',
-    version='0.1.0',
+    version='0.2.0',
     description='A forth-like programming language that runs on the'
     ' CPython VM',
     author='Joe Jevnik',
